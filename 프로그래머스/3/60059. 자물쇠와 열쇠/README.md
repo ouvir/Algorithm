49906775136a4aed32d3f0bd472e3c57bb41ceab
김경훈 <54941130+ouvir@users.noreply.github.com>
2024-07-29 18:23:34 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 640.70 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/README.md
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/README.md
@@ -1,10 +1,10 @@
 # [level 3] 자물쇠와 열쇠 - 60059 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60059) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60059#) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 608.29 ms
+메모리: 10.2 MB, 시간: 640.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 29일 18:18:42
+2024년 07월 29일 18:23:33
 
 ### 문제 설명
 
--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
@@ -1,40 +1,50 @@
-def check(n, new_lock):  # 가운데에 0인 부분이 있는지 체크
-    for i in range(n):
-        for j in range(n):
-            if new_lock[i + n][j + n] != 1: # 1이 아니면 자물쇠 못품
+def solution(key, lock):
+    for _ in range(4):
+        if check(key,lock):
+            return True
+        key = rotate(key)
+        
+    return False
+
+def rotate(key):
+    M = len(key[0])
+    output = [ [0] * (M) for _ in range(M) ]
+    
+    for i in range(M):
+        for j in range(M):
+            output[j][M-1-i] = key[i][j]
+            
+    return output
+    
+
+def check(key, lock): # key를 움직이며, 확인
+    N = len(lock[0])
+    M = len(key[0])
+    
+    array = [[0] * (3 * N) for _ in range(3 * N)]
+
+    for i in range(N):
+        for j in range(N):
+            array[i+N][j+N] = lock[i][j]
+
+    for mx in range(2*N):
+        for my in range(2*N):
+            for i in range(M):
+                for j in range(M):
+                    array[mx + i][my + j] += key[i][j]
+
+            if check_one(N, array):
+                return True
+
+            for i in range(M):
+                for j in range(M):
+                    array[mx + i][my + j] -= key[i][j]
+
+    return False
+
+def check_one(N, array):
+    for i in range(N):
+        for j in range(N):
+            if array[i + N][j + N] != 1:
                 return False
     return True
-
-def rotate(m, key):
-    new_arr = [[0] * m for _ in range(m)]
-    for i in range(m):
-        for j in range(m):
-            new_arr[i][j] = key[m - j - 1][i]
-
-    return new_arr
-
-def solution(key, lock):
-    n = len(lock)
-    m = len(key)
-
-    new_lock = [[0] * (n * 3) for _ in range(n * 3)] # 3배크기 배열 생성
-
-    for i in range(n):  # 가운데에 기존 lock 추가
-        for j in range(n):
-            new_lock[i + n][j + n] = lock[i][j]
-
-    for rot in range(4): # 회전 4번
-        key = rotate(m, key)
-        for x in range(2 * n):  # x축 이동
-            for y in range(2 * n):  # y축 이동
-                for i in range(m):  # 자물쇠에 열쇠 넣기
-                    for j in range(m):
-                        new_lock[x + i][y + j] += key[i][j]
-
-                if check(n, new_lock):
-                    return True
-                
-                for i in range(m):  # 자물쇠에 열쇠 빼기
-                    for j in range(m):
-                        new_lock[x + i][y + j] -= key[i][j]
-    return False
