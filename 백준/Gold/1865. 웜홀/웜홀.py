b5a33856a4cf2959e7d4508310a56034736356c4
김경훈 <54941130+ouvir@users.noreply.github.com>
2024-07-31 15:07:21 +0900

    [Gold III] Title: 웜홀, Time: 284 ms, Memory: 111628 KB -BaekjoonHub

---
+++ 백준/Gold/1865. 웜홀/웜홀.py
@@ -0,0 +1,37 @@
+import sys
+
+input = sys.stdin.readline
+INF = int(1e9)
+
+tc = int(input())
+
+def bf(distance, edges, V, E):
+    for i in range(V):
+        for j in range(E):
+            a, b, c = edges[j]
+            if distance[b] > distance[a] + c:
+                distance[b] = distance[a] + c
+                if i == V - 1:
+                    return True
+    return False
+
+for _ in range(tc):
+    N, M ,W = map(int,input().split())
+
+    graph = [[] for _ in range(N+1)]
+    edges = []
+    distance = [INF] * (N+1)
+
+    for __ in range(M):
+        a, b, c = map(int,input().split())
+        edges.append((a,b,c))
+        edges.append((b,a,c))
+    
+    for __ in range(W):
+        a, b, c = map(int,input().split())
+        edges.append((a,b,-c))
+
+    if bf(distance, edges, N, len(edges)):
+        print("YES")
+    else:
+        print("NO")
