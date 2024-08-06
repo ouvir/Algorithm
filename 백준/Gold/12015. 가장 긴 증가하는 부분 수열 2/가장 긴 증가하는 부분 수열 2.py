5e10f6d123118aaf768eee7992fdc0cbc3b97c80
김경훈 <54941130+ouvir@users.noreply.github.com>
2024-08-06 14:46:14 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 2236 ms, Memory: 155552 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -3,26 +3,28 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-
-A = list(map(int, input().split()))
+array = list(map(int, input().split()))
 
 LIS = [0]
 
-for a in A:
-    if a > LIS[-1]:
-        LIS.append(a)
-    else: # 이분탐색으로 들어갈 위치 탐색
-        start = 0
-        end = len(LIS)
+def binary_search(array, start, end):
+    output = end
 
-        while start < end:
-            mid = (start + end) // 2
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] >= n:
+            output = mid if mid < output else output
+            end = mid - 1
+        else:
+            start = mid + 1
 
-            if LIS[mid] < a:
-                start = mid + 1
-            else:
-                end = mid
+    return output 
 
-        LIS[end] = a
+for n in array:
+    if LIS[-1] < n:
+        LIS.append(n)
+    else:
+        index = binary_search(LIS, 0, len(LIS)-1)
+        LIS[index] = n
 
 print(len(LIS) - 1)
