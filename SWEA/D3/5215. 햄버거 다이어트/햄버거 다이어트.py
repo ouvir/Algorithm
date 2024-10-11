bb612279983700e568166aee1cac112018276059
김경훈 <54941130+ouvir@users.noreply.github.com>
2024-10-11 15:18:02 +0900

    [D3] Title: 햄버거 다이어트, Time: 270 ms, Memory: 62,448 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -2,18 +2,16 @@ T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
 	N, L = map(int, input().split())
-	items = [[0, 0]]
-	dp = [[0] * (L+1) for _ in range(N+1)] # 최대 이익을 저장
-    
-	for i in range(N):
-		items.append(list(map(int, input().split()))) # score, calorie
+	items = [[0,0]] # score, calorie
+	for _ in range(N):
+		items.append(list(map(int, input().split())))
+	dp = [[0] * (L+1) for _ in range(N+1)] # i번째 까지 item들로 만들어낼 수 있는 최고 점수
     
 	for i in range(1, N+1):
-		for j in range(1, L+1): #dp[i][j] = i번째 물건까지 있고, j 용량  만큼 담을 수 있을 때 최대 가치
-			weight = items[i][1]
-			value = items[i][0]
-			if j < weight: 
+		for j in range(1, L+1):
+			v, w = items[i]
+			if j < w:
 				dp[i][j] = dp[i-1][j]
 			else:
-				dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight] + value)
+				dp[i][j] = max(dp[i-1][j], dp[i-1][j-w] + v)
 	print(f"#{test_case} {dp[N][L]}")
