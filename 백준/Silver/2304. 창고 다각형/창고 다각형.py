86bf2ae22e4dc57c1fac3afeafc00aa465907967
김경훈 <54941130+ouvir@users.noreply.github.com>
2024-11-01 12:02:36 +0900

    [Silver II] Title: 창고 다각형, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.py
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.py
@@ -1,8 +1,7 @@
-# 위치 기준 정렬 > O(NlogN)
-# (다음 기둥 위치 - 현재 기둥 위치) * 현재 기둥 높이 = 면적
-# 가장 큰 기둥 기준 -> 왼쪽 오른쪽은 계속 감소하는 형태
-# 만약 오른쪽으로 진행하며, 
-# 감소가 아니라 커진 형태라면, 기존 기둥은 안쓰인다고 보면 됨
+# 높이 1 씩 증가시켜가며 계속 탐색
+# 탈출: (가장 큰 기둥 위치 == 가장 작은 기둥 위치)
+# 높이가 n 이상인 위치가 (가장 큰 기둥 - 가장 작은 기둥 위치 + 1 값) 을 더해주기
+# 이를 반복하며 탐색
 
 import sys
 
@@ -12,36 +11,26 @@ N = int(input())
 graph = []
 
 for _ in range(N):
-    L, H = map(int, input().split())
+    L, H = map(int,input().split())
     graph.append((L,H))
 
-graph.sort()
+graph.sort() # 위치 기준 정렬
 
-max_index = 0
-max_value = 0
-for i in range(N):
-    if max_value < graph[i][1]:
-        max_value = graph[i][1]
-        max_index = i
+height = 1
+start_idx = 0
+end_idx = N - 1
 
 output = 0
-h = graph[0][1]
-
-for i in range(max_index):
-    if graph[i+1][1] > h:
-        output += (graph[i+1][0] - graph[i][0]) * h
-        h = graph[i+1][1]
-    else:
-        output += (graph[i+1][0] - graph[i][0]) * h
+while True:
+    if start_idx > end_idx:
+        break
     
-output += graph[max_index][1]
-
-h = graph[N-1][1]
-for i in range(N-1, max_index, -1):
-    if graph[i-1][1] > h:
-        output += (graph[i][0] - graph[i-1][0]) * h
-        h = graph[i-1][1]
-    else:
-        output += (graph[i][0] - graph[i-1][0]) * h
-    
+    if graph[start_idx][1] < height:
+        start_idx += 1
+        continue
+    if graph[end_idx][1] < height:
+        end_idx -= 1
+        continue
+    output += (graph[end_idx][0] - graph[start_idx][0] + 1)
+    height += 1
 print(output)
