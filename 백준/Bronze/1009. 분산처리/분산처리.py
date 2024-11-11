77ef6af64c1e44911b281a52a30f75013960a539
김경훈 <54941130+ouvir@users.noreply.github.com>
2024-11-11 20:55:28 +0900

    [Bronze II] Title: 분산처리, Time: 1212 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.py
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -6,12 +6,13 @@ T = int(input())
 
 for _ in range(T):
     a, b = map(int, input().split())
-    a %= 10  # 마지막 자릿수만 고려하면 되므로 10으로 나눔
-    if b == 0:
-        tmp = 1
-    else:
-        exponent = b % 4 if b % 4 != 0 else 4
-        tmp = pow(a, exponent, 10)
+    tmp = a
+    tmp %= 10
+
+    for _ in range(b-1):
+        tmp *= a
+        tmp %= 10
+
     if tmp == 0:
         tmp = 10
     print(tmp)
