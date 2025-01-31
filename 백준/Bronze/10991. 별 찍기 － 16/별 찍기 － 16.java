3f76439733b089a19da2ec1c3e282dda25adaf1c
김경훈 <54941130+ouvir@users.noreply.github.com>
2025-01-31 19:42:52 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 292 ms, Memory: 22120 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    static int N;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N - i - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            for (int j = 0; j < i; j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
+}
