7448002bf6590f05ccabea95e5e7d7acbc4ea041
김경훈 <54941130+ouvir@users.noreply.github.com>
2025-03-13 00:33:43 +0900

    [Silver I] Title: 경로 찾기, Time: 136 ms, Memory: 13980 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+
+	static int[][] graph;
+	static int[][] output;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		N = Integer.parseInt(br.readLine());
+		graph = new int[N][N];
+		output = new int[N][N];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				graph[i][j] = Integer.parseInt(st.nextToken());
+				output[i][j] = graph[i][j];
+			}
+		}
+
+		for (int k = 0; k < N; k++) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if(graph[i][k] > 0 && graph[k][j] > 0) {
+						output[i][j] = 1;
+						graph[i][j] = 1;
+					}
+				}
+			}
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				sb.append(output[i][j]).append("\n");
+			}
+		}
+		System.out.print(sb);
+	}
+}
