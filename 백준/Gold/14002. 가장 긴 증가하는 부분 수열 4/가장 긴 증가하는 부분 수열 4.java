f4fe6183cbea64bd9b6767338a124041e6692531
김경훈 <54941130+ouvir@users.noreply.github.com>
2025-04-08 12:09:33 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 72 ms, Memory: 11932 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] nums = new int[N];
+		int[] lis = new int[N];
+		int[] idx = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		
+		int size = 0;
+		for (int i = 0; i < N; i++) {
+			int pos = Arrays.binarySearch(lis, 0, size, nums[i]);
+			idx[i] = pos;
+			if(pos >= 0) continue;
+			pos = Math.abs(pos) - 1;
+			lis[pos] = nums[i];
+			if(pos == size) size++;
+			idx[i] = pos;
+		}
+		
+		
+		// 역추적
+		int[] answer = new int[size];
+		int c = size-1;
+		
+		for (int i = N-1; i >= 0; i--) {
+			if(idx[i] == c) {
+				answer[c] = nums[i];
+				c--;
+			}
+		}
+		
+		sb.append(size).append("\n"); // size 
+		
+		for (int i = 0; i < size; i++) {
+			sb.append(answer[i]).append(" ");
+		}
+		
+		System.out.println(sb);
+	}
+}
