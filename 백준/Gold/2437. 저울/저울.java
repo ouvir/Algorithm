ee22d50c5c47f7c9d613bd2c5b19378283ed5b59
김경훈 <54941130+ouvir@users.noreply.github.com>
2025-04-08 14:24:14 +0900

    [Gold II] Title: 저울, Time: 72 ms, Memory: 11816 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] nums = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(nums);
+		
+		int sum = 0;
+		for (int i = 0; i < N; i++) {
+			if(nums[i] <= sum+1) {
+				sum += nums[i];
+			} else {
+				break;
+			}
+		}
+		System.out.println(sum+1);
+	}
+}
