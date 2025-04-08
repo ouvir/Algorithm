a37887d057ba4680bae21ddb490b2bc1a0f670d1
김경훈 <54941130+ouvir@users.noreply.github.com>
2025-04-08 13:32:54 +0900

    [Platinum V] Title: 전깃줄 - 2, Time: 524 ms, Memory: 50656 KB -BaekjoonHub

---
+++ 백준/Platinum/2568. 전깃줄 － 2/전깃줄 － 2.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		final int P = 500_000;
+		
+		int[] count = new int[P+1];
+		int[] nums = new int[N]; 
+		int[] LIS = new int[N]; // 이분 탐색용 배열
+		int[] IDX = new int[N]; // 역추적용 배열
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			count[b] = a;
+		}
+		
+		int c = 0;
+		for (int i = 0; i < P+1; i++) {
+			if(count[i] != 0) nums[c++] = count[i]; 
+		}
+		
+		
+		int size = 0;
+		for (int i = 0; i < N; i++) {
+			int pos = Arrays.binarySearch(LIS, 0, size,nums[i]);
+			if(pos >= 0) return;
+			
+			pos = Math.abs(pos) - 1;
+			
+			LIS[pos] = nums[i];
+			if(size == pos) {
+				size++;
+			}
+			IDX[i] = pos;
+		}
+		
+		c = size - 1;
+		List<Integer> target = new ArrayList<>();
+		
+		for (int i = N-1; i >= 0; i--) {
+			if(IDX[i] == c) {
+				c--;
+			} else {
+				target.add(nums[i]);
+			}
+		}
+		
+		sb.append(N-size).append("\n");
+		
+		Collections.sort(target);
+		
+		for (int t : target) {
+			sb.append(t).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
