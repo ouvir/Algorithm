2c031251257ebfb95af8a0a426ec16454d43129b
김경훈 <54941130+ouvir@users.noreply.github.com>
2025-04-09 14:04:16 +0900

    [Platinum V] Title: 전깃줄 - 2, Time: 204 ms, Memory: 24860 KB -BaekjoonHub

--- 백준/Platinum/2568. 전깃줄 － 2/전깃줄 － 2.java
+++ 백준/Platinum/2568. 전깃줄 － 2/전깃줄 － 2.java
@@ -1,16 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static int read() throws Exception {
+        int d, o;
+        boolean negative = false;
+        d = System.in.read();
+        if (d == '-') {
+            negative = true;
+            d = System.in.read();
+        }
+        o = d & 15;
+        while ((d = System.in.read()) > 32)
+            o = (o << 3) + (o << 1) + (d & 15);
+        return negative? -o:o;
+    }
+    
+	public static void main(String[] args) throws Exception {
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		int N = Integer.parseInt(br.readLine());
+		int N = read();
 		final int P = 500_000;
 		
 		int[] count = new int[P+1];
@@ -19,9 +26,8 @@ public class Main {
 		int[] IDX = new int[N]; // 역추적용 배열
 		
 		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
+			int a = read();
+			int b = read();
 			count[b] = a;
 		}
 		
