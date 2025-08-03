2466d0ffdf3c5facaf955dfa4e155208d034d29d
김경훈 <54941130+ouvir@users.noreply.github.com>
2025-08-03 13:43:52 +0900

    [Silver I] Title: 안전 영역, Time: 200 ms, Memory: 33836 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -5,61 +5,62 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static final int[] dx = {1, 0, -1, 0};
-	static final int[] dy = {0, 1, 0, -1};
-	static int N;
-	static int[][] graph;
-	static boolean[][] visited;
-	static int safeAreaValue;
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		N = Integer.parseInt(br.readLine());
-		
-		graph = new int[N][N];
-		safeAreaValue = Integer.MIN_VALUE;
-		
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				graph[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		for (int c = 0; c <= 100; c++) {
-			visited = new boolean[N][N];
-			int cnt = 0;
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					if(graph[i][j] > c && !visited[i][j]) {
-						bfs(i,j,c);
-						cnt++;
-					}
-				}
-			}
-			safeAreaValue = Math.max(safeAreaValue, cnt);
-		}
-		System.out.println(safeAreaValue);
-	}
+    private static final int MAX_HEIGHT = 100;
+    private static final int[] dx = new int[] {1, 0, -1, 0};
+    private static final int[] dy = new int[] {0, 1, 0, -1};
+    private static int N;
+    private static int[][] graph;
+    private static boolean[][] visited;
 
-	private static void bfs(int x, int y, int c) {
-		Queue<int[]> q = new ArrayDeque<>();
-		q.add(new int[]{x, y});
-		visited[x][y] = true;
-		
-		while(!q.isEmpty()) {
-			int[] cur = q.poll();
-			
-			for (int i = 0; i < 4; i++) {
-				int mx = cur[0] + dx[i];
-				int my = cur[1] + dy[i];
-				if(mx < 0 || mx >= N || my < 0 || my >= N) continue;
-				if(graph[mx][my] <= c || visited[mx][my]) continue;
-				visited[mx][my] = true;
-				q.add(new int[] {mx, my});
-			}
-		}
-	}
 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        graph = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int maxAreaCount = 0;
+        for (int h = 0; h <= MAX_HEIGHT; h++) {
+            int areaCount = 0;
+            visited = new boolean[N][N];
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if(graph[i][j] > h && !visited[i][j]) {
+                        bfs(i, j, h);
+                        areaCount++;
+                    }
+                }
+            }
+
+            maxAreaCount = Math.max(maxAreaCount, areaCount);
+        }
+        System.out.println(maxAreaCount);
+    }
+
+    private static void bfs(int i, int j, int h) {
+        Queue<int[]> q = new ArrayDeque<>();
+        q.add(new int[]{i, j});
+        visited[i][j] = true;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+
+            for(int d = 0; d < 4; d++) {
+                int mx =  cur[0] + dx[d];
+                int my =  cur[1] + dy[d];
+                if(mx < 0 || mx >= N || my < 0 || my >= N) continue;
+                if(visited[mx][my] || graph[mx][my] <= h) continue;
+                visited[mx][my] = true;
+                q.add(new int[]{mx, my});
+            }
+        }
+    }
 }
